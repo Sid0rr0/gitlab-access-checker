6f40b108591780002a6f7f9cfc6c9cbc01ea69c2
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-06-05 23:18:06 +0200

    feat: list of user groups & projects

--- app/components/GroupForm.tsx
+++ app/components/GroupForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import fetchGitLabAPI from '@/app/useGitLabAPI'
+import { getAllGroupsAndProjects } from '@/app/useGitLabAPI'
 import {
   useQuery,
 
@@ -10,7 +10,7 @@ import {
 export default function Home() {
   const [groupID, setGroupID] = useState<string | null>(null)
 
-  const query = useQuery({ queryKey: ['group-members'], queryFn: () => fetchGitLabAPI(`/groups/${groupID}/members`), enabled: !!groupID })
+  const query = useQuery({ queryKey: ['group-members'], queryFn: () => getAllGroupsAndProjects(Number(groupID)), enabled: !!groupID })
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -28,22 +28,46 @@ export default function Home() {
         </button>
       </form>
 
-      <div>
+      {query.isLoading && <p className="text-gray-500">Loading...</p>}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-4xl">
         {query.data?.map(user => (
-          <li key={user.id} className="bg-white p-4">
-            <span className="text-xl font-semibold text-gray-800">{user.name}</span>
-            {' '}
-            <a
-              href={user.web_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:text-blue-800 hover:underline text-lg"
-            >
-              (@
+          <div key={user.id} className="bg-white p-6 rounded-lg border border-gray-200">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">{user.name}</h2>
+            <p className="text-blue-600 text-lg mb-4">
+              @
               {user.username}
-              )
-            </a>
-          </li>
+            </p>
+
+            <div className="mb-4">
+              <h3 className="text-lg font-medium text-gray-700 mb-2">Groups:</h3>
+              {user.groups.length > 0
+                ? (
+                    <ul className="list-disc list-inside text-gray-600">
+                      {user.groups.map((group, index) => (
+                        <li key={index}>{group}</li>
+                      ))}
+                    </ul>
+                  )
+                : (
+                    <p className="text-gray-500 italic">No groups assigned</p>
+                  )}
+            </div>
+
+            <div>
+              <h3 className="text-lg font-medium text-gray-700 mb-2">Projects:</h3>
+              {user.projects.length > 0
+                ? (
+                    <ul className="list-disc list-inside text-gray-600">
+                      {user.projects.map((project, index) => (
+                        <li key={index}>{project}</li>
+                      ))}
+                    </ul>
+                  )
+                : (
+                    <p className="text-gray-500 italic">No projects assigned</p>
+                  )}
+            </div>
+          </div>
         ))}
       </div>
     </>
--- app/useGitLabAPI.ts
+++ app/useGitLabAPI.ts
@@ -7,3 +7,111 @@ export default async function callGitLabAPI(endpoint: string) {
 
   return await data.json()
 }
+
+type GitLabGroup = {
+  id: number
+  name: string
+}
+
+interface AccessLevelMapping {
+  [key: number]: string
+}
+
+const ACCESS_LEVELS: AccessLevelMapping = {
+  0: 'No access',
+  5: 'Minimal access',
+  10: 'Guest',
+  15: 'Planner',
+  20: 'Reporter',
+  30: 'Developer',
+  40: 'Maintainer',
+  50: 'Owner',
+  60: 'Admin',
+}
+
+type UserData = {
+  id: number
+  name: string
+  username: string
+  groups: string[]
+  projects: string[]
+}
+
+export async function getAllGroupsAndProjects(initialGroupId: number) {
+  const usersMap = new Map<number, UserData>()
+
+  const visitedGroups = new Set<number>()
+  const stack: GitLabGroup[] = []
+
+  stack.push(await getGroupDetails(initialGroupId))
+
+  while (stack.length > 0) {
+    const group = stack.pop() as GitLabGroup
+    if (visitedGroups.has(group.id)) continue
+
+    visitedGroups.add(group.id)
+    const members = await getGroupMembers(group.id)
+
+    for (const member of members || []) {
+      const existingUser = usersMap.get(member.id)
+      if (!existingUser) {
+        usersMap.set(member.id, {
+          id: member.id,
+          name: member.name,
+          username: member.username,
+          groups: [`${group.name} (${ACCESS_LEVELS[member.accessLevel]})`],
+          projects: [],
+        })
+      }
+      else {
+        existingUser.groups.push(`${group.name} (${ACCESS_LEVELS[member.accessLevel]})`)
+      }
+    }
+
+    const projects = await callGitLabAPI(`/groups/${group.id}/projects`)
+    for (const project of projects || []) {
+      const projectMembers = await getProjectMembers(project.id)
+      for (const projectMember of projectMembers || []) {
+        const existingUser = usersMap.get(projectMember.id)
+        if (existingUser) {
+          existingUser.projects.push(project.name)
+        }
+        else {
+          usersMap.set(projectMember.id, {
+            id: projectMember.id,
+            name: projectMember.name,
+            username: projectMember.username,
+            groups: [],
+            projects: [project.name],
+          })
+        }
+      }
+    }
+
+    const subgroups = await callGitLabAPI(`/groups/${group.id}/subgroups`)
+    for (const subgroup of subgroups || []) {
+      stack.push({ id: subgroup.id, name: subgroup.name })
+    }
+  }
+
+  console.log('Users Map:', usersMap)
+  return Array.from(usersMap.values())
+}
+
+async function getGroupDetails(groupId: number) {
+  const data = await callGitLabAPI(`/groups/${groupId}`)
+  return {
+    id: data.id,
+    name: data.name,
+  }
+}
+
+async function getGroupMembers(groupId: number) {
+  const data = await callGitLabAPI(`/groups/${groupId}/members`)
+  return data?.map(user => ({ id: user.id, name: user.name, username: user.username, accessLevel: user.access_level }))
+}
+
+async function getProjectMembers(projectId: number) {
+  const data = await callGitLabAPI(`/projects/${projectId}/members`)
+  return data?.map(user => ({ id: user.id, name: user.name, username: user.username, accessLevel: user.access_level }))
+}
