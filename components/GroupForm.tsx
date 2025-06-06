29cc08a4a9447ec2acced5b7e95971a07ee54f29
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-06-06 14:38:40 +0200

    feat: display total number of users

--- components/GroupForm.tsx
+++ components/GroupForm.tsx
@@ -127,6 +127,11 @@ export default function Home() {
           </div>
         ))}
       </div>
+      <span>
+        Total users:
+        {' '}
+        {query.data?.length || 0}
+      </span>
     </>
   )
 }
