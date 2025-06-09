9edefac97e080ed411d19f21fb1dbbc68c9abca8
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-06-09 13:36:19 +0200

    style: index page layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ const queryClient = new QueryClient()
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 items-center w-3xl">
           <h1 className="text-5xl font-bold">GitLab Access Checker</h1>
 
           <GroupForm />
